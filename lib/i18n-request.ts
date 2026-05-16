2483563d79283e6ad803e7f1170b144ce00b16d3
Abdulaziz <aalaqeel03@gmail.com>
2026-05-17 01:38:19 +0300

    fix: add locale to next-intl request config

--- lib/i18n-request.ts
+++ lib/i18n-request.ts
@@ -1,10 +1,8 @@
 import { getRequestConfig } from 'next-intl/server'
-import { getMessages, isLocale, defaultLocale } from './i18n'
 
 export default getRequestConfig(async ({ locale }) => {
-  const validLocale = isLocale(locale) ? locale : defaultLocale
   return {
-    locale: validLocale,
-    messages: await getMessages(validLocale)
+    locale: locale as string,
+    messages: (await import(`./messages/${locale}.json`)).default
   }
 })
