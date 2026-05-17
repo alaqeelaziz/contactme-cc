a089c35e30aaa688fc720cb89f4be40fa0911d95
Abdulaziz <aalaqeel03@gmail.com>
2026-05-18 00:03:57 +0300

    fix: update root i18n-request with switch

--- i18n-request.ts
+++ i18n-request.ts
@@ -1,8 +1,7 @@
-import { getRequestConfig } from 'next-intl/server'
+﻿import { getRequestConfig } from 'next-intl/server'
 
 export default getRequestConfig(async ({ locale }) => {
   const safeLocale = ['ar', 'en', 'zh', 'fr', 'es'].includes(locale as string) ? locale as string : 'ar'
-  
   let messages
   switch (safeLocale) {
     case 'en': messages = (await import('./messages/en.json')).default; break
@@ -11,6 +10,5 @@ export default getRequestConfig(async ({ locale }) => {
     case 'es': messages = (await import('./messages/es.json')).default; break
     default:   messages = (await import('./messages/ar.json')).default
   }
-
   return { locale: safeLocale, messages }
 })
