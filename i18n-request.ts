20563da8a1cf7d954b738c585e464d7fb5869ebe
Abdulaziz <aalaqeel03@gmail.com>
2026-05-17 14:08:19 +0300

    fix: sync i18n-request to root

--- i18n-request.ts
+++ i18n-request.ts
@@ -1,8 +1,16 @@
-﻿import { getRequestConfig } from 'next-intl/server'
+import { getRequestConfig } from 'next-intl/server'
 
 export default getRequestConfig(async ({ locale }) => {
-  return {
-    locale: locale as string,
-    messages: (await import('./messages/' + locale + '.json')).default
+  const safeLocale = ['ar', 'en', 'zh', 'fr', 'es'].includes(locale as string) ? locale as string : 'ar'
+  
+  let messages
+  switch (safeLocale) {
+    case 'en': messages = (await import('./messages/en.json')).default; break
+    case 'zh': messages = (await import('./messages/zh.json')).default; break
+    case 'fr': messages = (await import('./messages/fr.json')).default; break
+    case 'es': messages = (await import('./messages/es.json')).default; break
+    default:   messages = (await import('./messages/ar.json')).default
   }
+
+  return { locale: safeLocale, messages }
 })
