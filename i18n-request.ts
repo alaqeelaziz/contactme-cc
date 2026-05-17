1fd0501ec9a770e569591483c8c8fa1a77c0d0c7
Abdulaziz <aalaqeel03@gmail.com>
2026-05-17 12:49:11 +0300

    fix: i18n-request with locale field in root

--- i18n-request.ts
+++ i18n-request.ts
@@ -1,8 +1,8 @@
-import { getRequestConfig } from 'next-intl/server'
+﻿import { getRequestConfig } from 'next-intl/server'
 
 export default getRequestConfig(async ({ locale }) => {
   return {
     locale: locale as string,
-    messages: (await import(`./messages/${locale}.json`)).default
+    messages: (await import('./messages/' + locale + '.json')).default
   }
 })
